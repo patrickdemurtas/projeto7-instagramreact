39e4b51870e6ecaedb6ba4d5ed8eb62643304bf0
patrickdemurtas <patrickmurtass@gmail.com>
2022-11-21 11:50:56 -0300

    separacao inicial

---
+++ src/components/Stories.js
@@ -0,0 +1,29 @@
+export default function Stories() {
+    return (
+        <div class="stories">
+            <div class="story">
+                <div class="imagem">
+                    <img src="assets/img/9gag.svg" />
+                </div>
+                <div class="usuario">
+                    9gag
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="assets/img/meowed.svg" />
+                </div>
+                <div class="usuario">
+                    meowed
+                </div>
+            </div>
+
+
+
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    )
+}
